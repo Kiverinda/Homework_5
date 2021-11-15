b95cd298fbc8b1709fb9cb56d3266517a808831b
KiverinDmitry <kiverinda@yandex.ru>
2021-11-15 22:48:52 +0500

    homework

---
+++ BytesStreamProcessing/WriteToTXT.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using Microsoft.Extensions.Options;
+using Homework_5.Emulator;
+using System.Threading.Tasks;
+
+namespace Homework_5.BytesStreamProcessing
+{
+    class WriteToTXT : AbstractHandler
+    {
+        private string _pathToTxt;
+
+        public WriteToTXT(IOptions<AppSettings> appSettings)
+        {
+            _pathToTxt = appSettings.Value.PathToWriteTXT;
+        }
+
+        public async override Task<object> Handle(object request)
+        {
+            if (request is DataTXT)
+            {
+                var source = request as IData;
+                var data = source.GetData().Data;
+
+                using (FileStream SourceStream = File.Open(_pathToTxt, FileMode.OpenOrCreate))
+                {
+                    await SourceStream.WriteAsync(data, 0, data.Length);
+                }
+                return null;
+            }
+            else
+            {
+                return base.Handle(request);
+            }
+        }
+    }
+}
---
+++ Emulator/DataTXT.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Options;
+
+namespace Homework_5.Emulator
+{
+    class DataTXT : IData
+    {
+        private string _pathToRead;
+        private readonly Log _logger;
+        private DataModel _data;
+
+        public DataTXT(IOptions<AppSettings> appSettings, Log loger)
+        {
+            _pathToRead = appSettings.Value.PathToReadFile;
+            _logger = loger;
+            _data = SetData();
+        }
+
+        private DataModel SetData()
+        {
+            try
+            {
+                using (FileStream fsSource = new FileStream(_pathToRead, FileMode.Open, FileAccess.Read))
+                {
+                    byte[] bytes = new byte[fsSource.Length];
+                    fsSource.Read(bytes);
+                    return new DataModel(bytes, RandomFloatValue(), RandomFloatValue());
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                _logger.LogWrite(ex.Message);
+            }
+
+            return null;
+        }
+
+        private float RandomFloatValue()
+        {
+            Random randomize = new Random();
+            return randomize.Next(0, 101);
+        }
+
+        public DataModel GetData()
+        {
+            return _data;
+        }
+
+        private void View()
+        {
+            foreach (byte b in _data.Data)
+            {
+                Console.WriteLine(b);
+            }
+        }
+    }
+}
